37319d4fbd3f93a2c7a5cf5af65c388a517b28e2
FilippaSKillick <filippa.h.k@gmail.com>
2026-02-03 22:03:19 +0100

    testing run

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello universe!'
+    return 'Hello universe this is my new pipeline!'
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
